0781e724c46ffe2f1ddac2378f3f72ca91c98dc8
Mikkel Norsgaard <mnt@integration-it.com>
2024-03-04 11:32:06 +0100

    Added first Timer Trigger Function App

---
+++ DanskMetal.FirstFunctionApp/DanskMetal.FirstFunctionApp/DanskMetalFirstTimerTriggerFunction.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace DanskMetal.FirstFunctionApp
+{
+    public class DanskMetalFirstTimerTriggerFunction
+    {
+        private readonly ILogger _logger;
+
+        public DanskMetalFirstTimerTriggerFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DanskMetalFirstTimerTriggerFunction>();
+        }
+
+        [Function("DanskMetalFirstTimerTriggerFunction")]
+        public void Run([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"Dansk Metal Timer Trigger executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
